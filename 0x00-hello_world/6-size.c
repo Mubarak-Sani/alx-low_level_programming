494fa15e9809d165d9a0c8f435f73409bfa7684c
MubaraqSani <mubaraq.sani@e4email.net>
2022-03-10 17:35:04 +0100

    Size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-	printf("Size of a char:%lu ", sizeof(char), "byte(s)");
+	printf("Size of a char:%lu\n ", sizeof(char), "byte(s)");
 	printf("Size of an int:%lu\n ", sizeof(int), "byte(s)");
 	printf("Size of a long int:%lu\n ", sizeof(long), "byte(s)");
 	printf("Size of a long long int:%lu\n ", sizeof(long long), "byte(s)");
