8971aa5e2b2f3e938ae2d294bf3493ea5f86f5b3
MubaraqSani <mubaraq.sani@e4email.net>
2022-03-12 08:50:12 +0100

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print alphabets without the use of printf.
+ * Return: 0
+ */
+int main(void)
+{
+    char str = 'abcdefghijklmnopqrstuvwxyz';
+
+    putchar(str);
+}
