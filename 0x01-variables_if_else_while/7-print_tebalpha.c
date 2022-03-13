0b0c484fc1833d2828dfdc1d5e58a75877e1adfd
MubaraqSani <mubaraq.sani@e4email.net>
2022-03-13 09:32:35 +0100

    print alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebalpha.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print the alphabet in a reverse form.
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
