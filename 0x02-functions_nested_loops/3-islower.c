8eef4cefc3bb75fe8a0941ec6b991a2d6c93cf8d
MubaraqSani <mubaraq.sani@e4email.net>
2022-03-15 22:01:33 +0100

    check for lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _islower - checks for lower cases letters
+ * @brief: input.
+ *
+ * Return 1 if c is lowercase.
+ * 0 if anything else
+ */
+
+int islower(c)
+{
+    if (c >= 'a' && c <= 'z')
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
+}
