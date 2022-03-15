b09fcf918acb70082b866c856c37429a1ba5556f
MubaraqSani <mubaraq.sani@e4email.net>
2022-03-15 16:54:54 +0100

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,10 +5,12 @@
  * description - a function that prints alphabet
  * Return: Always 0.
  */
+
 void print_alphabet(void)
 {
 	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
+
 	for (i = 0; i < 26; i++)
 {
 	_putchar(alphabet[i]);
