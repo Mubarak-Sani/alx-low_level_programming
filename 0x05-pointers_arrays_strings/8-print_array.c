ae43d796fd57af47486731e2e929bbeb464280e2
MubaraqSani <mubaraq.sani@e4email.net>
2022-03-21 18:01:28 +0100

    Pointers, arrays, strings: print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * void print_array - prints n elements of an array of integers, followed by a new line.
+ * print_array - prints n elements of an array of integers.
  *
  * @a: pointer to a.
  * @n: int n
