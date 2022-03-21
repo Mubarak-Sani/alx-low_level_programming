5a81100e8bba7a71083c350b42d4646ba7979c64
MubaraqSani <mubaraq.sani@e4email.net>
2022-03-21 16:30:02 +0100

    Pointers, arrays, strings: _putchar

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,11 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * _puts: prints a string, followed by a newline.
  * @str: pointer to the argument.
  * Return: Always (0)
  * 
  */
-
 void _puts(char *str)
 {
     int i = 0;
