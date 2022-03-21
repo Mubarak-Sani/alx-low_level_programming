e478fbd1890057b22509cf98fe7cfee5a2aa076a
MubaraqSani <mubaraq.sani@e4email.net>
2022-03-21 15:32:33 +0100

    Pointers, arrays, strings: swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,12 +2,14 @@
 
 /**
  * swap_int: swaps the values of two integers.
- *
+ * @a: pointer to one int.
+ * @b: pointer to another(second) int.
  * Return: void.
  */
-
 void swap_int(int *a, int *b)
 {
+	int temp = *a;
+
 	*a = 42;
 	*b = 98;
 }
