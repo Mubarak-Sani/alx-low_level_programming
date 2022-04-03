9c86b40dafe81b448dd1d188a5a3b23816a2d707
MubaraqSani <mubaraq.sani@e4email.net>
2022-04-03 13:17:52 +0100

    Static libraries

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - prints a string, followed by a newline.
+ * @str: pointer to the argument.
+ * Return: Always (0)
+ *
+ */
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (*str != '\0')
+	{
+		putchar(*str);
+		i++;
+		str++;
+	}
+	putchar('\n');
+}
